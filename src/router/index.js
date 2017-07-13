00718507ed029b2b7e5979cdc43306a29f9b4b04
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-07-13 19:48:47 +0200

    Make routes for the signin component

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import SignIn from '@/components/SignIn'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      // for now lets treat the he
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
     }
   ]
 })
