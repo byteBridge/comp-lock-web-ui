58bc7e3cd655857d9a4d339239b835dfbc42d96e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-08 18:14:44 +0200

    Add a toolbar with a help button leading to /help

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app light>
+    <v-toolbar dark class="teal darken-2">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Computer Lock</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon :to="{ path: '/help' }">
+        <v-icon>help</v-icon>
+      </v-btn>
+    </v-toolbar>
   <router-view></router-view>
   </v-app>
 </template>
