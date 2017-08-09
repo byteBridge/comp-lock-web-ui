6aa053bd7eb7a93ab5ce0432e63f0b488f576de1
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-09 10:20:15 +0200

    Show a signin button on the toolbar
     -- only show if user is unauthenticated
         and if he/she is not @ the /signin route

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,31 @@
       <v-btn icon :to="{ path: '/help' }">
         <v-icon>help</v-icon>
       </v-btn>
+      <v-btn
+        :to="signinUrl"
+        v-if="authenticated === false && $route.path !=='/signin'"
+      >
+        SIGN IN
+      </v-btn>
     </v-toolbar>
   <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-  export default { }
+  export default {
+    computed: {
+      signinUrl () {
+        return { path: '/signin' }
+      },
+
+      authenticated () {
+        const user = JSON.parse(localStorage.getItem('user'))
+        const token = user ? user.token : null
+        return !!token
+      }
+    }
+  }
 </script>
 
 <style lang="stylus">
