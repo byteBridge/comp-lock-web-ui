9377d5647bc6ca5be7468a240210ea21db37a1d5
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-13 18:08:29 +0200

    Add overview and settings placeholders

---
+++ src/components/AdminOverview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Hello there, this is the overview page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AdminSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Hey there this is the settings page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
