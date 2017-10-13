8081dbaf2bbfcbbbc9fcbf86c439b5b337935b32
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-13 18:03:17 +0200

    [admin.vue] make the admin component show its child components

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-      <p>Hello admin. Only authenticated people can see this</p>
-      <v-card>
-          <v-card-text>
-              <v-list>
-                  <v-list-tile v-for = "user in users" :key="user.username">
-                      <p>{{ user.f_name }} {{ user.s_name }}</p> <v-chip small>{{user.type}}</v-chip>
-                  </v-list-tile>
-              </v-list>
-          </v-card-text>
-      </v-card>
+    <v-container>
+      <router-view></router-view>
+    </v-container>
   </div>
 </template>
 
