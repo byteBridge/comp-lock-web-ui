932d07d7fb5afcce8c9ad6ce898dac33f81edabd
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-15 11:14:44 +0200

    Show the user activities and history in the AdminViewAccount

--- src/components/AdminViewAccount.vue
+++ src/components/AdminViewAccount.vue
@@ -1,20 +1,106 @@
 <template>
   <div>
-    hey you are viewing {{$route.params.username}}'s account
-    {{user}}
+    <v-toolbar class="mt-5 teal--text">
+      <v-icon class="teal--text">perm_identity</v-icon>
+        {{user.f_name}} {{ user.s_name}}
+      <v-chip small label outline class="teal">{{user.type}}</v-chip>
+      <v-spacer></v-spacer>
+      <v-btn class="elevation-1 orange white--text"><v-icon>block</v-icon>Block</v-btn>
+      <v-btn class="elevation-1 red white--text"><v-icon>delete</v-icon>Delete</v-btn>
+    </v-toolbar>
+  
+  <v-card v-if="user.history && user.history.length > 1"  class="mt-2" style="height:300px;">
+      <v-btn-toggle v-model.number="daysBack" style="float:right;">
+        <v-btn class="teal--text" flat :value="7">
+          Past week
+        </v-btn>
+        <v-btn class="teal--text"  flat :value="14">
+          Past 2 weeks
+        </v-btn>
+        <v-btn class="teal--text"  flat :value="30">
+          Past month
+        </v-btn>
+        <v-btn class="teal--text"  flat :value="90">
+          past 3 months
+        </v-btn>
+      </v-btn-toggle>
+    <line-chart :height="110" :data="user.history" :daysBack="daysBack"></line-chart>
+  </v-card>
+   <v-card  v-if="user.history && user.history.length > 1" class="mt-2">
+     <v-card-title>
+       History
+       <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        v-model="searchText"
+      ></v-text-field>
+     </v-card-title>
+     <v-card-text>
+       <v-data-table
+          v-bind:headers="headers"
+          :items="user.history"
+          :search="searchText"
+          v-bind:pagination.sync="pagination"
+          hide-actions
+          class="elevation-1"
+        >
+        <template slot="items" scope="props" @click="alert('hello world')">
+          <td class="text-xs-left">{{ props.item.computer_name }}</td>
+          <td class="text-xs-left">{{ props.item.log_date }}</td>
+          <td class="text-xs-right">{{ props.item.start_time }}</td>
+          <td class="text-xs-right">{{ props.item.duration }}</td>
+        </template>
+        <template slot="pageText" scope="{ pageStart, pageStop }">
+          From {{ pageStart }} to {{ pageStop }}
+        </template>
+        </v-data-table>
+        <div class="text-xs-center pt-2">
+          <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+        </div>
+     </v-card-text>
+   </v-card>
+
+    <v-card class="mt-2" v-else>
+      <v-card-text>{{user.f_name}} {{user.s_name}}'s account has never used to access the computers.</v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
+import LineChart from '@/components/LineChart.js'
 export default {
+  components: { LineChart },
   data () {
-    return { user: {} }
+    return {
+      user: {},
+      headers: [
+        { text: 'Computer Name', value: 'computer_name', align: 'left' },
+        { text: 'Date', value: 'log_date', align: 'left' },
+        { text: 'Time', value: 'start_time' },
+        { text: 'Duration', value: 'duration' }
+      ],
+      pagination: {},
+      searchText: '',
+      daysBackList: [
+        { title: 'Past week', value: 7 },
+        { title: 'Past two weeks', value: 14 },
+        { title: 'Past month', value: 30 },
+        { title: 'Past three months', value: 90 }
+      ],
+      daysBack: 7
+    }
   },
   mounted () {
     this.$http
       .get(`/users/${this.$route.params.username}/history?token=${this.$store.getters.authUser.token}`)
       .then(res => {
         this.user = res.data.user
+      })
+      .catch(res => {
+        console.log(res.status)
       })
   }
 }
