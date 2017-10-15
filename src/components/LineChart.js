f004d0d0158d64167c86d34c50a96b68156b09f8
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-15 11:14:05 +0200

    Create a line chart component

---
+++ src/components/LineChart.js
@@ -0,0 +1,75 @@
+import { Line } from 'vue-chartjs'
+import moment from 'moment'
+
+function newDate (days) {
+  return moment().add(days, 'd').format('MM/DD/YYYY')
+}
+
+function normaliseData (rawData, daysBack) {
+  let labels = []
+  var minutes = 0
+  var data = []
+
+  // iterate from back in time to today. e.g 7 for i week, 30 for a month, etc
+  for (let i = -daysBack; i < 1; i++) {
+    labels.push(newDate(i))
+  }
+
+  // find matching days to the days on the label and plot their minutes values on the graph
+  labels.forEach(dayLogged => {
+    const matchingDays = rawData.filter(x => x.log_date === dayLogged)
+
+    // calculate the minutes used for each day
+    if (matchingDays.length === 0) minutes = 0
+    if (matchingDays.length > 0) matchingDays.forEach(h => { minutes += Number(h.duration.slice(3, 5)) })
+
+    // push the minutes to be plotted on graph
+    data.push(minutes)
+  })
+
+  return {
+    labels,
+    datasets: [{
+      label: 'Login activities',
+      borderColor: 'teal',
+      backgroundColor: 'rgba(224, 242, 241, 0.6)',
+      data
+    }]
+  }
+}
+
+export default {
+  extends: Line,
+  props: ['data', 'daysBack'],
+  watch: {
+    daysBack (value) { this.renderChart(normaliseData(this.data, Number(this.daysBack)), this.chartConfig) }
+  },
+  data () {
+    return {
+      chartConfig: {
+        legend: {
+          display: true,
+          position: 'bottom'
+        },
+        elements: {
+          line: {
+            tension: 0 // disables bezier curves
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              max: 30 // to be replaced by the time limits from api
+            }
+          }],
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(normaliseData(this.data, Number(this.daysBack)), this.chartConfig)
+  }
+}
