8cbad5af8186724d4f49a8baeace477a4f55beed
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-19 05:18:12 +0200

    Tweak the view accounts view

--- src/components/ViewAccounts.vue
+++ src/components/ViewAccounts.vue
@@ -1,15 +1,21 @@
 <template>
   <v-card>
+ 
     <v-card-title>
-      User Accounts
-      <v-spacer></v-spacer>
+      <v-toolbar dark dense class="teal">
+    <v-toolbar-title>User Accounts</v-toolbar-title>
+    <v-spacer></v-spacer>
       <v-text-field
+        class="white"
+        style="border-radius:2px;"
         append-icon="search"
         label="Search"
         single-line
         hide-details
         v-model="searchText"
       ></v-text-field>
+  </v-toolbar>
+    
     </v-card-title>
     <v-card-text>
     <v-data-table
@@ -20,12 +26,19 @@
       class="elevation-1"
     >
     <template slot="items" scope="props" @click="alert('hello world')">
-      <td style="width: 20px;"><v-icon>perm_identity</v-icon></td>
-      <td class="text-xs-left">{{ props.item.f_name }}</td>
-      <td class="text-xs-left">{{ props.item.s_name }}</td>
-      <td class="text-xs-right"><v-icon :class="{ 'red--text': props.item.blocked === true, 'green--text':props.item.blocked===false}">fiber_manual_record</v-icon></td>
+      <td class="text-xs-left"><v-btn :to="{ path: `/admin/users/${props.item.username}` }" flat class="teal--text"> <v-icon>open_in_new</v-icon> {{ props.item.s_name }} {{ props.item.f_name }}</v-btn></td>
+      <td class="text-xs-left">{{ props.item.type }}</td>
       <td class="text-xs-right">{{ props.item.gender }}</td>
-      <td><v-btn :to="{ path: `/admin/users/${props.item.username}` }" flat class="teal--text"><v-icon>open_in_new</v-icon> View Details</v-btn></td>
+      <td class="text-xs-right">
+        <v-chip v-if="props.item.blocked === true" text-color="red" color="red" outline label>Blocked</v-chip>
+        <v-chip v-if="props.item.blocked === false" text-color="green" color="green" outline label>Not Blocked</v-chip>
+      </td>
+      <td>
+        <v-btn small icon><v-icon>print</v-icon></v-btn>
+        <v-btn small icon v-if="props.item.blocked"><v-icon>check</v-icon></v-btn>
+        <v-btn small icon v-if="!props.item.blocked"><v-icon>block</v-icon></v-btn>
+        <v-btn small icon><v-icon>delete</v-icon></v-btn>
+      </td>
     </template>
     </v-data-table>
     </v-card-text>
@@ -37,11 +50,11 @@
     data () {
       return {
         headers: [
-          { text: '', value: '' },
-          { text: 'Name', value: 'f_name' },
-          { text: 'Surname', value: 's_name' },
+          { text: 'Name', value: 's_name', align: 'left' },
+          { text: 'Account Type', value: 'type', align: 'left' },
+          { text: 'Gender', value: 'gender' },
           { text: 'Blocked Status', value: 'blocked' },
-          { text: 'Gender', value: 'gender' }
+          { text: 'Actions', align: 'left' }
         ],
         users: [],
         searchText: ''
