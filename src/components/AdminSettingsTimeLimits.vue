506ea38d4aafa23a88a1e6bd73820fed11aeb5e6
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-20 11:04:38 +0200

    Implement the appropriate props and methods to be able edit time limit

--- src/components/AdminSettingsTimeLimits.vue
+++ src/components/AdminSettingsTimeLimits.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
+        <!-- Show the dialog to add/update timelimits -->
+        <AppNewAccountType
+          :show="userTypeDialog.show"
+          :isEdit="userTypeDialog.isEdit"
+          :editUserType="userTypeDialog.userType"
+          @close="getUserTypes">
+        </AppNewAccountType>
+
         <v-card>
             <v-card-title>
                 <v-toolbar dense dark class="teal">
                     <v-toolbar-title><v-icon dark>timer</v-icon> Time limits</v-toolbar-title>
                     <v-spacer></v-spacer>
-                    <AppNewAccountType @close="getUserTypes"></AppNewAccountType>
+                      <v-btn dark class="green" @click="showUserTypeDialog({}, false)"><v-icon>add</v-icon>New account type</v-btn>
               </v-toolbar>
             </v-card-title>
 
@@ -21,8 +29,9 @@
                     <td class="text-xs-left"> {{ props.item.user_type }}</td>
                     <td class="text-xs-left">{{ props.item.time_limit }}</td>
                     <td>
-                       <v-btn small icon><v-icon>edit</v-icon></v-btn>
-                      <v-btn small icon><v-icon>delete</v-icon></v-btn>
+                       <v-btn
+                       block flat
+                       @click="showUserTypeDialog(props.item, true)"><v-icon left >edit</v-icon> edit</v-btn>
                     </td>
                 </template>
                 </v-data-table>
@@ -39,12 +48,15 @@
       return {
         headers: [
           { text: 'Account Type', value: 'user_type', align: 'left' },
-          { text: 'Time limit', value: 'gender', align: 'left' },
-          { text: 'Actions', align: 'left' }
+          { text: 'Time limit', value: 'gender', align: 'left' }
         ],
         userTypes: [],
         searchText: '',
-        dialog: false
+        userTypeDialog: {
+          show: false,
+          isEdit: false,
+          userType: {} // user_type, time_limit
+        }
       }
     },
     mounted () {
@@ -52,11 +64,20 @@
     },
     methods: {
       getUserTypes () {
+        // just to reset
+        this.userTypeDialog.show = false
+
         this.$http.get('/users/timelimits')
         .then(res => {
           this.userTypes = res.data.data
         })
         .catch(res => console.log(res))
+      },
+
+      showUserTypeDialog ({ user_type, time_limit }, isEdit) {
+        this.userTypeDialog.isEdit = !!isEdit
+        this.userTypeDialog.userType = { user_type, time_limit }
+        this.userTypeDialog.show = true
       }
     }
   }
