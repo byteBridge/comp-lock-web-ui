dab34a23c5ee5d17c521328198b1c9f1b8ba9e02
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-01-31 22:33:37 +0200

    Add feature to clear online user
      -- only if the account has been online
        for at least 4 hours.

--- src/components/AdminOnlineAccounts.vue
+++ src/components/AdminOnlineAccounts.vue
@@ -35,6 +35,12 @@
           <td class="text-xs-left">{{ props.item.type }}</td>
           <td class="text-xs-left">{{ props.item.computer_name }}</td>
           <td class="text-xs-left">{{ props.item.login_date | niceDate }}</td>
+          <td class="text-xs-left" v-if="hoursFrom(props.item.login_date) >= 4 || daysFrom(props.item.login_date) > 0">
+            <v-tooltip top>  
+              <v-btn slot="activator" @click="clearOnlineUser(props.item.username)" dark small><v-icon class="">clear</v-icon> Clear</v-btn>
+              <span>Sometimes a power cut occurs and when a student tries to login they are told their account is already logged in. This button only shows up when an account has been online for at least 4 hours.</span>
+            </v-tooltip>
+          </td>
         </template>
        </v-data-table>
      </v-card-text>
@@ -69,6 +75,23 @@
     filters: {
       niceDate (value) {
         return moment().to(value)
+      }
+    },
+
+    methods: {
+      hoursFrom (date) {
+        return moment(moment().diff(moment(date))).hour()
+      },
+
+      daysFrom (date) {
+        return moment(moment().diff(moment(date))).day()
+      },
+
+      clearOnlineUser (username) {
+        this.$http.put(`/users/online/${username}/clear`)
+          .then(response => {
+            this.users.forEach((user, i) => (user.username === username) && this.users.splice(i, 1))
+          })
       }
     }
   }
